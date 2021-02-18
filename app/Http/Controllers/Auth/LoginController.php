2278df2299a1877fbe881e090ecb38a8d8206839
Muhammad Farras hakim <farrashakim@gmail.com>
2021-02-18 14:51:13 +0700

    Middleware in Login,Register, and Dashboard Controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['guest']);
+    }
+
     public function index()
     {
         return view('auth.login');
@@ -14,12 +19,13 @@ class LoginController extends Controller
 
     public function store(Request $request)
     {
+
         $this->validate($request, [
             'email' => 'required|email|max:255',
             'password' => 'required',
         ]);
 
-        if (!auth()->attempt($request->only('email', 'password'))) {
+        if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
             return back()->with('status', 'Invalid login credentials');
         }
 
